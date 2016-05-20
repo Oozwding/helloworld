6c8f15f98caca91ebd39170107a2d87b3385b5fa
Oozwding <zwdingcoder@gmail.com>
2016-05-20 17:20:58 +0800

    自动化提交test

---
+++ PAT/test.cpp
@@ -0,0 +1,23 @@
+/**
+ *   @Author: zwding
+ *   @Created Time : 2016年05月17日 星期二 19时02分45秒
+ *   @Last Modified: 2016年05月17日 星期二 19时19分21秒
+ *   @File Name: test.cpp
+ *   @Description:
+**/
+#include<iostream>
+#include<vector>
+using namespace std;
+class node{
+public:
+	int x;
+	node(int x1,int y1):x(x1),y(y1){}
+	void test(int);
+private:
+	int y;
+}
+void node
+int main(){
+
+	return 0;
+}
