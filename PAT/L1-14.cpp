f37287efe0edf5a0c7f530f90f3b8afb03414fa6
Oozwding <zwdingcoder@gmail.com>
2016-05-21 09:39:08 +0800

    L1-14-题

---
+++ PAT/L1-14.cpp
@@ -0,0 +1,26 @@
+/**
+ *   @Author: zwding
+ *   @Created Time : 2016年05月21日 星期六 08时53分14秒
+ *   @Last Modified: 2016年05月21日 星期六 08时54分46秒
+ *   @File Name: L1-14.cpp
+ *   @Description:
+**/
+
+#include<iostream>
+#include<vector>
+#include<deque>
+#include<string>
+#include<set>
+#include<map>
+#include<queue>
+#include<list>
+#include<stack>
+#include<algorithm>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	cout << "This is a simple problem." << endl;
+}
